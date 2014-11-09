80125b8347cdc6cc92136c9f039374bb42e98ac0
peteruithoven <peter@peteruithoven.nl>
2014-11-10 00:12:26 +0100

    Fakeprinter improvements
    - Auto register
    - Storing data
    - Disconnect from all sockets
    - Cleanup

--- fakeprinter/index.js
+++ fakeprinter/index.js
@@ -1,24 +1,26 @@
+var io = require('socket.io-client');
 var keypress = require('keypress');
-var debug = require('debug')('fakeprinter');
+var request = require('request');
 var spawn = require('child_process').spawn;
 var ss = require('socket.io-stream');
-var fs = require('fs');
+var LocalStorage = require('node-localstorage').LocalStorage;
+var localStorage = new LocalStorage('./localStorage');
 
-//var PORT = process.env.PORT ? process.env.PORT : 5000;
 //var CLOUD_URL = "https://cloud.doodle3d.com";
-var CLOUD_URL = "http://localhost:5001";
-var printer = require("./mock/printer")(CLOUD_URL);
-var printerID;
-var printerKey;
-var nspPrinterRoot;
-var nspPrinterPrinter;
+var CLOUD_URL = "http://localhost:5000";
+var APP_NAME = "localprinters-fakeprinter";
+var pridNSP;
+var printerNSP;
+var webcamNSP;
+var snapshotTimeout;
+var sockets = [];
 
 //var printerKey = '545d34c344204e5e34eb07ceggWuT64If0TNO3nVSbMd';
-var printerKey = '545fd2549b049b00003aec98hTVMaddHoEfTWBSuG1IL';
+var printerKey; //= '545fd2549b049b00003aec98hTVMaddHoEfTWBSuG1IL';
 //var printerID = '545d34c344204e5e34eb07ce';
-var printerID = '545fd2549b049b00003aec98';
+var printerID; // = '545fd2549b049b00003aec98';
 
-//printer.register(function(err,printerKey,printerID) {
+//register(function(err,printerKey,printerID) {
 // console.log(err,printerKey,printerID);
 // process.exit();
 //});
@@ -32,16 +34,9 @@ if (process.env['_']=='/opt/local/bin/nodemon') {
   // make `process.stdin` begin emitting "keypress" events
   keypress(process.stdin);
   process.stdin.setRawMode(true);
-  process.stdin.resume();
-  connect();
+  process.stdin.resume();  
 }
-
-  
-function setState(state) {
-  console.log(!!nspPrinterRoot,!!nspPrinterRoot,state);
-  if (nspPrinterRoot) nspPrinterRoot.emit("printerState", {state: state});
-  if (nspPrinterPrinter) nspPrinterPrinter.emit("state", {state: state});
-}
+connect();
 
 process.stdin.on('keypress', function (ch, key) {
   //console.log('got "keypress"', key);
@@ -57,11 +52,13 @@ process.stdin.on('keypress', function (ch, key) {
       setState('idle');
       break;
     case 'd':
-      console.log("nspPrinterRoot disconnect");
-      nspPrinterRoot.disconnect();
+      console.log("disconnect");
+      for(var i in sockets) {
+        sockets[i].disconnect();
+      }
       break;
     case 'r':
-      console.log("nspPrinterRoot + nspPrinterPrinter reconnect");
+      console.log("reconnect");
       connect();
       break;
     case 'c':
@@ -71,61 +68,103 @@ process.stdin.on('keypress', function (ch, key) {
 });
 
 function connect() {
-  printer.connectTo("/",printerKey,{forceNew:true},function(err,nsp) {
-    if(err) throw new Error(err);
+  var appData = localStorage.getItem(APP_NAME);
+  console.log("appData: ",appData);
+  if(appData === null) return register();
+  appData = JSON.parse(appData);
+  printerKey = appData.printerKey;
+  printerID = appData.printerID;
+  connectTo("/",function(err,nsp) {
+    if(err) {
+      if(err === "Printer unknown") register();
+      else throw new Error(err);
+    }
     setTimeout(function() {
-      
-      printer.connectTo("/"+printerID,printerKey,{forceNew:true},function(err,nsp) {
+      connectTo("/"+printerID,function(err,nsp) {
         if(err) throw new Error(err);
-        nspPrinterRoot = nsp;
-
+        pridNSP = nsp;
         console.log("fake /"+printerID + " emit printerState & clientSSID");
-
         // nsp.emit("printerState", {state: "idle"});
         setState('idle');
         nsp.emit("clientSSID",{ssid:"Vechtclub XL F1.19"});
       });  
-
-      printer.connectTo("/"+printerID+"-printer",printerKey,{forceNew:true},function(err,nsp) {
+      connectTo("/"+printerID+"-printer",function(err,nsp) {
         if (err) throw new Error(err);
-        // if (!nsp) throw new Error("Error /"+printerID+"-printer namespace is not ready");
-
-        nspPrinterPrinter = nsp;
-
+        printerNSP = nsp;
         nsp.on("print",function(data,cb,stream) {
           console.log("onPrint",data,cb,stream);
           setState('printing');
           if (cb) cb(null,"ok");
         });
-
         nsp.on("stop",function(data,cb,stream) {
           console.log("onStop",data,cb,stream);
           if (cb) cb(null,"ok");
           setState('idle');
         });
       });
-
-      printer.connectTo("/"+printerID+"-webcam",printerKey,{forceNew:true},function(err,nsp) {
+      connectTo("/"+printerID+"-webcam",function(err,nsp) {
         if (err) throw new Error(err);
+        webcamNSP = nsp;
         takeSnapshot();
-        var snapshotTimeout;
-        function takeSnapshot() {
-          console.log('tick cam');
-          var snapshot = spawn('imagesnap', ['-'])
-          var convert = spawn('convert', ['-', '-quality', '80', '-resize', '640x360', 'JPEG:-']);
-          var stream = ss.createStream();
-          ss(nsp).emit('image', stream); 
-          snapshot.stdout.pipe(convert.stdin);
-          convert.stdout.pipe(stream);
-          convert.stdout.on("end",function() {
-            console.log("convert end");
-            clearTimeout(snapshotTimeout);
-            takeSnapshot();
-          });
-          clearTimeout(snapshotTimeout);
-          snapshotTimeout = setTimeout(takeSnapshot,5000);
-        }
       });
     },500);
-  });  
+  });
+}
+
+function takeSnapshot() {
+  console.log('taking snapshot');
+  if(!webcamNSP || !webcamNSP.connected) return;
+  var snapshot = spawn('imagesnap', ['-'])
+  var convert = spawn('convert', ['-', '-quality', '80', '-resize', '640x360', 'JPEG:-']);
+  var stream = ss.createStream();
+  ss(webcamNSP).emit('image', stream); 
+  snapshot.stdout.pipe(convert.stdin);
+  convert.stdout.pipe(stream);
+  convert.stdout.on("end",function() {
+    console.log("  snapshot was send");
+    clearTimeout(snapshotTimeout);
+    takeSnapshot();
+  });
+  clearTimeout(snapshotTimeout);
+  snapshotTimeout = setTimeout(takeSnapshot,5000);
+}
+function setState(state) {
+  console.log(!!pridNSP,!!printerNSP,state);
+  if (pridNSP) pridNSP.emit("printerState", {state: state});
+  if (printerNSP) printerNSP.emit("state", {state: state});
+}
+
+function connectTo(nsp,callback) {
+  var nspURL = CLOUD_URL+nsp+'?key='+printerKey;
+  nspURL += "&type=printer";
+  var socket = io.connect(nspURL, {forceNew:true});
+  socket.once('connect',function() {
+    console.log(nsp+": connected");
+    if(callback) callback(null,socket);
+  });
+  socket.once('error',function(err) {
+    console.log(nsp+": error: ",err);
+    if(callback) callback(err);
+  });
+  sockets.push(socket);
+  return socket;
+}
+function register() {
+  console.log("register");
+  var json = {
+    name: APP_NAME,
+    features: ["printer","webcam","slice"]
+  };
+  request.post({url:CLOUD_URL+"/printer/register", json:json}, function(err, httpResponse, body) {
+    if(err) throw new Error("printer register error: "+err);
+    if(typeof body !== "object") {
+      throw new Error("invalid register response: ",body);
+    }
+    var appData = {
+      printerKey:body.key,
+      printerID: body.id
+    };
+    localStorage.setItem(APP_NAME,JSON.stringify(appData));
+    connect();
+  });
 }
